36a5038fa31497c28480fcd3a40f897c45e3a7cd
Olga Butolina <obutolin@c2r8s7.42prague.com>
2026-01-26 12:10:03 +0100

    module1

---
+++ module1/ex03/calledwhatsyourname.py
@@ -0,0 +1,18 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    calledwhatsyourname.py                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: obutolin <obutolin@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/01/26 12:01:01 by obutolin          #+#    #+#              #
+#    Updated: 2026/01/26 12:08:54 by obutolin         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+first_name = 'Olga'
+last_name = 'Butolina'
+whole_name = first_name + ' ' + last_name
+print("Hey, what's your first name? :", first_name)
+print("And your last name? :", last_name)
+print("Well, pleased to meet you, %s." % whole_name)
